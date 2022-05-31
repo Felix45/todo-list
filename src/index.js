c8ff7861cb1b220ad67b5473fa7b7461911fc797
Felix Ouma <fatonoh@gmail.com>
2022-06-01 00:06:00 +0300

    Add project source files

--- src/index.js
+++ src/index.js
@@ -1,18 +1,39 @@
 import _ from 'lodash';
 import printMe from './print.js';
+import '@fortawesome/fontawesome-free/js/all'
 import './style.css';
 
-const component = () => {
-  const element = document.createElement('div');
-  const btn = document.createElement('button');
+const todos = [
+  {
+    id: 1,
+    description: "Wash the dishes",
+    completed: false
+  },
+  {
+    id: 2,
+    description: "complete todo list project",
+    completed: false
+  },
+  {
+    id: 3,
+    description: "Attend football training",
+    completed: false
+  },
+  {
+    id: 4,
+    description: "Wash utensils after football practice",
+    completed: false
+  },
+];
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  btn.innerHTML = 'Click me and check the console';
-  btn.onclick = printMe;
-
-  element.classList.add('hello');
-
-  element.appendChild(btn);
-  return element;
+const printTodoList = () => {
+  const listContainer = document.querySelector('.todo-list');
+  todos.forEach((todo) => {
+    listContainer.innerHTML += `<li class='d-flex p-2'>
+      <input type='checkbox' id=${todo.id} />
+      <label class='desc px-1' for=${todo.id}>${todo.description}</label>
+      <span class='fa fa-ellipsis-v fa-lg'></span>
+    </li>`;
+  })
 };
-document.body.appendChild(component());
+printTodoList();
--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,14 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
 };
