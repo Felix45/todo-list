f63549f5de468ac69ee76e7680056c34e68e940e
Felix Ouma <fatonoh@gmail.com>
2022-06-01 01:15:31 +0300

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,28 +1,26 @@
-import _ from 'lodash';
-import printMe from './print.js';
-import '@fortawesome/fontawesome-free/js/all'
+import '@fortawesome/fontawesome-free/js/all.js';
 import './style.css';
 
 const todos = [
   {
     index: 1,
-    description: "Wash the dishes",
-    completed: false
+    description: 'Wash the dishes',
+    completed: false,
   },
   {
     index: 2,
-    description: "complete todo list project",
-    completed: false
+    description: 'complete todo list project',
+    completed: false,
   },
   {
     index: 3,
-    description: "Attend football training",
-    completed: false
+    description: 'Attend football training',
+    completed: false,
   },
   {
     index: 4,
-    description: "Wash utensils after football practice",
-    completed: false
+    description: 'Wash utensils after football practice',
+    completed: false,
   },
 ];
 
@@ -34,6 +32,6 @@ const printTodoList = () => {
       <input type='text' class='desc font-normal px-1' value='${todo.description}' disabled/>
       <span class='fa fa-ellipsis-v fa-lg'></span>
     </li>`;
-  })
+  });
 };
 printTodoList();
