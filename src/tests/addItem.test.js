1f485e554b6f3e802757b56f80fb109644001e07
Felix Ouma <fatonoh@gmail.com>
2022-06-08 23:54:33 +0300

    Add tests for addTask method in class TaskStore

---
+++ src/tests/addItem.test.js
@@ -0,0 +1,28 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import TaskStore from '../taskStore.js';
+
+describe('Todo list addItem tests', () => {
+  const taskstore = new TaskStore();
+  document.body.innerHTML = '<div><ul class="todo-list"></ul></div>';
+
+  test('add Item I', () => {
+    taskstore.addTask('play football');
+    const list = document.querySelectorAll('.todo-list li');
+    expect(list).toHaveLength(1);
+  });
+
+  test('add Item II', () => {
+    taskstore.addTask('play tennis');
+    const list = document.querySelectorAll('.todo-list li');
+    expect(list).toHaveLength(2);
+  });
+
+  test('add Item III', () => {
+    taskstore.addTask('Watch fally ipupa concert');
+    const list = document.querySelectorAll('.todo-list li');
+    expect(list).toHaveLength(3);
+  });
+});
