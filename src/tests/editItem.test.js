12ea987580b2635b280514bd29f28ac502f207dc
ibrahim777764 <ibrahimnaseer896@gmail.com>
2022-06-10 14:09:07 +0500

    Add unit test of clear completed todo

--- src/tests/editItem.test.js
+++ src/tests/editItem.test.js
@@ -40,3 +40,20 @@ describe('set completed status', () => {
     expect(statusTwo).toBe(true);
   });
 });
+
+describe('clear completed tasks', () => {
+  test('clear all todos', () => {
+    taskstore.removeTask(true, -1);
+    let list = document.querySelectorAll('.todo-list li');
+    expect(list).toHaveLength(0);
+    taskstore.addTask('go swimming');
+    taskstore.addTask('go hiking');
+    taskstore.addTask('prepare dinner');
+    taskstore.setTaskStaus(0);
+    taskstore.setTaskStaus(1);
+    taskstore.setTaskStaus(2);
+    taskstore.removeTask(true, -1);
+    list = document.querySelectorAll('.todo-list li');
+    expect(list).toHaveLength(0);
+  });
+});
